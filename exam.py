ff5726f9b88dab8b52cc9994d5dec5da711c502a
Viktor Khankishyiev <vitya99111@gmail.com>
2020-12-18 11:44:03 +0200

    test

---
+++ exam.py
@@ -0,0 +1,20 @@
+class Progression:
+    def __init__(self, n):
+        self.n = n
+        self.a = 3
+        self.b = 5
+
+    def summa(self):
+        if self.n <= 0:
+            raise ValueError
+        s = (self.n * (2 * self.a + self.b * (self.n - 1))) / 2
+        return s
+
+
+def main():
+    progression = Progression(10)
+    print('Сума перших 10 елементів: {summa}'.format(summa=progression.summa()))
+
+
+if __name__ == '__main__':
+    main()
---
+++ test.py
@@ -0,0 +1,23 @@
+import unittest
+from exam import Progression
+
+
+class TestProgression(unittest.TestCase):
+    test = Progression(20)
+    test2 = Progression(75)
+    test3 = Progression(3)
+    test4 = Progression(40)
+    test_error = Progression(-1)
+
+
+
+    def test_summa(self):
+        self.assertRaises(ValueError,self.test_error.summa)
+        self.assertEqual(self.test.summa(), 1010)
+        self.assertTrue(self.test.summa(), isinstance(self.test.summa(), int))
+        self.assertEqual(self.test2.summa(), 14100)
+        self.assertEqual(self.test3.summa(), 24)
+        self.assertEqual(self.test4.summa(), 4020)
+
+if __name__ == '__main__':
+    unittest.main()
